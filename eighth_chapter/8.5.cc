97871fbcce584c137cd6d9fae947b64ea779ff07
logic <13572186526@163.com>
2018-05-07 09:41:37 +0800

    o

---
+++ eighth_chapter/8.5.cc
@@ -0,0 +1,18 @@
+void merge(int R[], int low, int mid, int high);
+
+/**
+ * 二路归并排序
+ * 时间复杂度：O(nlog以2为底的n)
+ * 空间复杂度：O(n)
+ * @param R
+ * @param low
+ * @param high
+ */
+void mergeSort(int R[], int low, int high) {
+    if (low < high) {
+        int mid = (low + high) / 2;
+        mergeSort(R, low, mid);
+        mergeSort(R, mid + 1, high);
+        merge(R, low, mid, high);
+    }
+}
